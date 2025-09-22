42c031c2280b262cdf6f29827ba42abab04b9786
Ghadachihi99 <ghada.chihi@outlook.com>
2025-09-22 09:32:38 +0200

    adding cookies

--- pythonbuilding/carsharing.py
+++ pythonbuilding/carsharing.py
@@ -3,8 +3,6 @@ from fastapi import FastAPI, HTTPException, Depends, Request
 from typing import Annotated
 
 from fastapi.responses import JSONResponse
-
-
 from pythonbuilding.db import get_session
 from pythonbuilding.schemas import Car, CarOutput, TripOutput, TripInput, Trip
 from sqlmodel import SQLModel, Field, Session, select
@@ -22,8 +20,8 @@ app = FastAPI(title="Car Sharing", lifespan=lifespan)
 app.include_router(car.router)
 app.include_router(web.router)
 
-@app.exception_handler(BadTripException)
-async def unicorn_exception_handler(request: Request, exc : BadTripException):
+@app.exception_handler(car.BadTripException)
+async def unicorn_exception_handler(request: Request, exc : car.BadTripException):
     return JSONResponse(
         status_code=statistics.HTTP_422_UNPROCESSABLE_ENTITY,
         content={"message": "Bad Trip"}, 
@@ -47,3 +45,4 @@ def add_trip(session: Annotated[Session, Depends(get_session)],
 async def get_all_cars(session: Session = Depends(get_session)):
     trips = session.exec(select(Trip)).all()
     return [TripOutput.model_validate(t) for t in trips]
+
