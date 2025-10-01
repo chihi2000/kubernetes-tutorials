cacb9f70336a5508d4cc1fde4c06a571276ef9a9
Ghadachihi99 <ghada.chihi@outlook.com>
2025-10-01 17:42:43 +0200

    added deploy pipeline

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -15,3 +15,21 @@ variable "project_name" {
   type        = string
   default     = "carsharing"
 }
+
+variable "acr_name" {
+  description = "Name of the Azure Container Registry (must be globally unique)"
+  type        = string
+  # No default - must be provided in terraform.tfvars
+}
+
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+  default     = "rg-carsharing-tutorial"
+}
+
+variable "aks_cluster_name" {
+  description = "Name of the AKS cluster"
+  type        = string
+  default     = "aks-carsharing-tutorial"
+}
